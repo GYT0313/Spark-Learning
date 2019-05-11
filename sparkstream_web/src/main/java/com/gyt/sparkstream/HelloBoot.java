929a69cc98344ade739d7d9f30b5a045488d7663
hadoop <guyongtao@qq.com>
2019-05-11 18:43:51 +0800

    Spark Streaming流处理后构建Spring Boot可视化

---
+++ sparkstream_web/src/main/java/com/gyt/sparkstream/HelloBoot.java
@@ -0,0 +1,15 @@
+package com.gyt.sparkstream;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloBoot {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello() {
+        return "Hello Word...";
+    }
+
+}
